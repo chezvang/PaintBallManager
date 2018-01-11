eea2d4987f4a47fd6b6db1796a7f8db6d590863d
Chez <chez.vang@gmail.com>
2018-01-11 10:37:45 -0600

    initial project commit

---
+++ PaintBallManager/PaintBallManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaintBallManager.MvcApplication" Language="C#" %>
---
+++ PaintBallManager/PaintBallManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PaintBallManager.Startup))]
+namespace PaintBallManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
